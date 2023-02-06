c42904f69ddcaaf360e535cc1a54344602bef98e
Evgenii Elchev <yolchev@gmail.com>
2023-02-06 18:58:37 +0700

    first constant

--- BaseIosApp/App/MyApp.swift
+++ BaseIosApp/App/MyApp.swift
@@ -3,9 +3,11 @@ import SwiftUI
 @main
 struct MyApp: App {
     var body: some Scene {
+        let stringFirst = "Строка 1"
+        
         return WindowGroup {
             return VStack {
-                Text("Строка 1")
+                Text(stringFirst)
                 Text("Строка 2")
                 Text("Строка 3")
                 Text("Строка 4")
