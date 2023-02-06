76aaca452d7bb5fa13355ad5f95d95d72c717247
Evgenii Elchev <yolchev@gmail.com>
2023-02-06 18:55:23 +0700

    stack

--- BaseIosApp/App/MyApp.swift
+++ BaseIosApp/App/MyApp.swift
@@ -4,7 +4,14 @@ import SwiftUI
 struct MyApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VStack {
+                Text("Строка 1")
+                Text("Строка 2")
+                Text("Строка 3")
+                Text("Строка 4")
+                Text("Строка 5")
+                Text("Строка 6")
+            }
         }
     }
 }
